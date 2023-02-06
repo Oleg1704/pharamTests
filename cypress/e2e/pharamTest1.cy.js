de56cdebb57c5ec8abcde92c4f17064e2134cab5
“Oleg <olegbosyuk5@gmail.com>
2023-02-06 15:35:48 +0200

    Added four files with homework

---
+++ cypress/e2e/pharamTest1.cy.js
@@ -0,0 +1,48 @@
+describe('Test1', () => {
+    
+    const paramTest = ({testData, testData1, time}) =>
+    
+          function () {
+            cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
+            cy.get(':nth-child(3) > nb-card-body > .theme-preview').click();
+            cy.get('a.ng-tns-c141-19').click();
+            cy.get('.menu-item.ng-tns-c141-23').click();
+            cy.get('.theme-select > .select-button').click();
+            cy.get('#nb-option-23')
+            cy.get('.eva-menu-outline').click()
+            
+  
+            cy.get('.content .row .col-md-6:first-of-type  .select-button').click();
+            cy.get('#nb-option-24')
+            .should('have.attr','ng-reflect-value');
+            cy.get('#nb-option-24').click()
+  
+            cy.get('input[ng-reflect-name="title"]').clear().type(`${testData}`)
+            .should('have.attr', 'ng-reflect-model');
+            cy.get('input[ng-reflect-name="content"]').clear().type(`${testData1}`)
+            .should('have.attr','ng-reflect-model');
+  
+  
+            cy.get('input[ng-reflect-name="timeout"]').clear().type(`${time}`)
+            .should('have.attr','ng-reflect-model')
+  
+            cy.get('.content .row .col-md-6:last-of-type  .select-button').click();
+            cy.get('#nb-option-32').click();
+  
+            cy.get('nb-card-footer button:first-of-type').click();
+  
+            cy.get('nb-toastr-container.ng-tns-c209-54 > .ng-tns-c209-54')
+            .should('have.css', 'background-color')
+            .and('eq', 'rgb(233, 29, 99)')
+  
+            cy.get('nb-toastr-container.ng-tns-c209-54 > .ng-tns-c209-54 span')
+            .should('have.class', 'title')
+  
+            cy.get('nb-toastr-container.ng-tns-c209-54 > .ng-tns-c209-54 div')
+            .should('have.class', 'message')
+            
+          }
+    
+    it('input title and content', paramTest({testData: 'Hello world',testData1: 'I am the best Qa', time: '10000'}));
+        
+    });
